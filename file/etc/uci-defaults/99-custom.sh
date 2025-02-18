d42c9f78f2506aa7ceded38e9b71f781f47e98ea
zhz8888 <zhz1021@gmail.com>
2025-02-19 00:44:31 +0800

    feat: 更新代码 - 删除PPPoE拨号信息预设 - 删除部分软件以释放空间 - 首次运行脚本添加修改软件镜像源 - 设置仅LAN口设备可访问网页终端和SSH - 删除Action权限设置

--- file/etc/uci-defaults/99-custom.sh
+++ file/etc/uci-defaults/99-custom.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 该脚本为immortalwrt首次启动时 运行的脚本 即 /etc/uci-defaults/99-custom.sh
+# 该脚本为 immortalwrt 首次启动时运行的脚本，即 /etc/uci-defaults/99-custom.sh
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 uci set firewall.@zone[1].input='ACCEPT'
 
@@ -8,42 +8,25 @@ uci add dhcp domain
 uci set "dhcp.@domain[-1].name=time.android.com"
 uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
-# 检查配置文件是否存在
-SETTINGS_FILE="/etc/config/pppoe-settings"
-if [ ! -f "$SETTINGS_FILE" ]; then
-    echo "PPPoE settings file not found. Skipping." >> $LOGFILE
-else
-   # 读取pppoe信息(由build.sh写入)
-   . "$SETTINGS_FILE"
-fi
-# 无需判断网卡数量
+# 设置 DHCP 默认网关为 192.168.100.1
 uci set network.lan.ipaddr='192.168.100.1'
 echo "set 192.168.100.1 at $(date)" >> $LOGFILE
-# 判断是否启用 PPPoE
-echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
-if [ "$enable_pppoe" = "yes" ]; then
-    echo "PPPoE is enabled at $(date)" >> $LOGFILE
-    # 设置拨号信息
-    uci set network.wan.proto='pppoe'                
-    uci set network.wan.username=$pppoe_account     
-    uci set network.wan.password=$pppoe_password     
-    uci set network.wan.peerdns='1'                  
-    uci set network.wan.auto='1' 
-    echo "PPPoE configuration completed successfully." >> $LOGFILE
-else
-    echo "PPPoE is not enabled. Skipping configuration." >> $LOGFILE
-fi
 
-# 设置所有网口可访问网页终端
-uci delete ttyd.@ttyd[0].interface
+# 设置仅 LAN 口可访问网页终端
+uci set ttyd.@ttyd[0].interface='lan wlan0 wlan1'
 
-# 设置所有网口可连接 SSH
-uci set dropbear.@dropbear[0].Interface=''
+# 设置仅 LAN 口可连接 SSH
+uci set dropbear.@dropbear[0].Interface='lan wlan0 wlan1'
 uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
 NEW_DESCRIPTION="Compiled by zhz8888"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
+
+# 设置默认软件源
+sed -e 's,https://downloads.immortalwrt.org,https://mirrors.cernet.edu.cn/immortalwrt,g' \
+    -e 's,https://mirrors.vsean.net/openwrt,https://mirrors.cernet.edu.cn/immortalwrt,g' \
+    -i.bak /etc/opkg/distfeeds.conf
 
 exit 0
--- jdcloud/ramips/build-24.sh
+++ jdcloud/ramips/build-24.sh
@@ -2,18 +2,6 @@
 # 该文件实际为imagebuilder容器内的build.sh
 # yml 传入的路由器型号 PROFILE
 echo "Building for profile: $PROFILE"
-echo "Create pppoe-settings"
-mkdir -p  /home/build/immortalwrt/files/etc/config
-
-# 创建pppoe配置文件 yml传入pppoe变量————>pppoe-settings文件
-cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
-enable_pppoe=${ENABLE_PPPOE}
-pppoe_account=${PPPOE_ACCOUNT}
-pppoe_password=${PPPOE_PASSWORD}
-EOF
-
-echo "cat pppoe-settings"
-cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
@@ -23,16 +11,16 @@ PACKAGES=""
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 # 服务——FileBrowser 用户名admin 密码admin
-PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 #24.10.0
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+# PACKAGES="$PACKAGES luci-app-openclash"
+# PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
